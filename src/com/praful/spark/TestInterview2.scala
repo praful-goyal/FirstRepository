84a49717b309f80fb9114771c2286d0c090c1c3b
Pulkit Goyal <ecstatic.praful@gmail.com>
2019-11-02 09:10:34 +1100

    initial commit

---
+++ src/com/praful/spark/TestInterview2.scala
@@ -0,0 +1,15 @@
+package com.praful.spark
+
+object TestInterview2 {
+  
+  def main(args: Array[String]) {
+    
+    val Array(brokers, topics) = args
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+    println(topicsSet)
+    println(kafkaParams)
+    
+  }
+  
+}
